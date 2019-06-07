318e2c73aa4a395d4f8ed995f9ad6ca32d7bd591
Luka Salonen <luka.salonen@protonmail.com>
2019-06-07 14:06:33 +0300

    Unicafe exercises

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Title = (props) => {
+    return (
+        <div>
+            <h1>
+                {props.name}
+            </h1>
+        </div>
+    )
+}
+
+const Button = (props) => {
+    return (
+        <button onClick={props.handleClick}>
+            {props.text}
+        </button>
+    )
+}
+
+const Statistic = ({text, stat, extra}) => {
+    if(!extra) {
+        return (
+            <tr>
+                <td>{text}</td>
+                <td>{stat}</td>
+            </tr>
+        )
+    } else {
+        return (
+            <tr>
+                <td>{text}</td>
+                <td>{stat}</td>
+                <td>{extra}</td>
+            </tr>
+        )
+    }
+}
+
+const Statistics = ({good,neutral,bad}) => {
+
+    const all = () => good + neutral + bad
+    const positive = () => (good * 100) / all()
+    const average = () => (good - bad) / all()
+
+    if(all() === 0) {
+        return (
+            <div>
+                <Title name="statistics" />
+                No feedback given
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <Title name="statistics" />
+                <table>
+                    <tbody>
+                        <Statistic text="good" stat={good} />
+                        <Statistic text="neutral" stat={neutral} />
+                        <Statistic text="bad" stat={bad} />
+                        <Statistic text="all" stat={all()} />
+                        <Statistic text="average" stat={average()} />
+                        <Statistic text="positive" stat={positive()} extra="%" />
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  return (
+    <div>
+      <Title name="give feedback" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
