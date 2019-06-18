d13b2ee411a285096e860d2841959811b404c66f
Luka Salonen <luka.salonen@protonmail.com>
2019-06-18 15:23:45 +0300

    Osa3ab exercises

---
+++ osa3/puhelinluettelo-backend/build/precache-manifest.28146fb1f39502f92f9c676f07f394ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a597034828ebe38aa50878d4e872f38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b7b3585530604f9ba44",
+    "url": "/static/css/main.5a4a57dc.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "4b7b3585530604f9ba44",
+    "url": "/static/js/main.a518582d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
