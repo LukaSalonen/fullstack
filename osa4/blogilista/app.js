01e651e92cae290f2cacd7f0f60546e319aac630
Luka Salonen <luka.salonen@protonmail.com>
2019-06-26 17:12:38 +0300

    Osa4b exerciese

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,11 +5,15 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
 mongoose.connect(config.mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,22 +2,48 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 
-blogsRouter.get('/', (request, response) => {
+blogsRouter.get('/', (request, response, next) => {
     Blog
       .find({})
       .then(blogs => {
-        response.json(blogs)
+        response.json(blogs.map(blog => blog.toJSON()))
+      })
+      .catch(error => {
+        error
       })
   })
   
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', (request, response, next) => {
     const blog = new Blog(request.body)
   
     blog
       .save()
       .then(result => {
-        response.status(201).json(result)
+        response.status(201).json(result.toJSON())
+      })
+      .catch(error => {
+        next(error)
       })
   })
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const blog = request.body
+
+  try {
+    await Blog.findByIdAndUpdate(request.params.id, blog, { new: true})
+    response.status(200).end()
+  } catch(error) {
+    next(error)
+  }
+})
 
   module.exports = blogsRouter
